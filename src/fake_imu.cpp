94068d567f21e0e1bd793da46279fda74a2af63a
jlurz <jlurz@osboxes.(none)>
2016-07-10 10:34:41 -0400

    Initial commit

---
+++ src/fake_imu.cpp
@@ -0,0 +1,91 @@
+#include <ros/ros.h>
+#include <sensor_msgs/Imu.h>
+#include <gazebo_msgs/GetModelState.h>
+#include <message_filters/subscriber.h>
+#include <geometry_msgs/PoseStamped.h>
+
+namespace {
+using namespace std;
+using namespace geometry_msgs;
+
+static const double FREQUENCY = 0.001;
+
+class FakeIMU {
+private:
+    //! Publisher for the pose
+    ros::Publisher posePub;
+
+	//! Node handle
+	ros::NodeHandle nh;
+
+	//! Private nh
+	ros::NodeHandle pnh;
+
+    //! Frequency at which to publish the humanoid state
+    ros::Timer timer;
+
+    //! Cached service client.
+    ros::ServiceClient modelStateServ;
+
+    //! Publisher for the pose visualization
+    ros::Publisher poseVizPub;
+
+    //! Model name
+    string modelName;
+
+public:
+	FakeIMU() :
+		pnh("~") {
+        posePub = nh.advertise<sensor_msgs::Imu>(
+				"out", 1);
+
+        ros::service::waitForService("/gazebo/get_model_state");
+        modelStateServ = nh.serviceClient<gazebo_msgs::GetModelState>("/gazebo/get_model_state", true /* persistent */);
+        pnh.param<string>("modelName", modelName, "human");
+        poseVizPub = nh.advertise<geometry_msgs::PoseStamped>("imu/pose", 1);
+
+        timer = nh.createTimer(ros::Duration(FREQUENCY), &FakeIMU::callback, this);
+        timer.start();
+	}
+
+private:
+
+    void visualizeOrientation(const std_msgs::Header& header, const geometry_msgs::Quaternion& orientation) {
+        PoseStamped ps;
+        ps.header = header;
+        ps.pose.orientation = orientation;
+        poseVizPub.publish(ps);
+    }
+
+    sensor_msgs::Imu getIMUData(){
+        gazebo_msgs::GetModelState modelState;
+        modelState.request.model_name = modelName;
+        modelStateServ.call(modelState);
+        sensor_msgs::Imu data;
+        data.header.stamp = ros::Time::now();
+        data.header.frame_id = "/odom_combined";
+
+        data.angular_velocity = modelState.response.twist.angular;
+        data.orientation = modelState.response.pose.orientation;
+
+        return data;
+    }
+
+    void callback(const ros::TimerEvent& event){
+
+        // Lookup the current IMU data for the human
+        sensor_msgs::Imu data = getIMUData();
+        visualizeOrientation(data.header, data.orientation);
+
+        // Publish the event
+        ROS_DEBUG_STREAM("Publishing an IMU event: " << data);
+        posePub.publish(data);
+      }
+};
+}
+int main(int argc, char** argv) {
+	ros::init(argc, argv, "fake_imu");
+
+	FakeIMU fi;
+	ros::spin();
+}
---
+++ src/random_torque_applier.cpp
@@ -0,0 +1,147 @@
+#include <ros/ros.h>
+#include <gazebo_msgs/ApplyBodyWrench.h>
+#include <geometry_msgs/PoseStamped.h>
+#include <boost/random.hpp>
+#define USE_FIXED_SEED 1
+
+namespace
+{
+
+using namespace std;
+using namespace ros;
+
+static const double DURATION_DEFAULT = 0.001;
+static const unsigned int FIXED_SEED = 0;
+
+class RandomTorqueApplier
+{
+private:
+
+    //! Node handle
+    ros::NodeHandle nh;
+
+    //! Private nh
+    ros::NodeHandle pnh;
+
+    //! Gazebo client
+    ros::ServiceClient gazeboClient;
+
+    //! Notifier
+    ros::Publisher notifier;
+
+    //! X torque
+    double x;
+
+    //! Y torque
+    double y;
+
+    //! Z torque
+    double z;
+
+    //! Torque Duration
+    double duration;
+
+    //! Set random values for x-y
+    bool random;
+
+    //! Wait for a specific topic
+    bool waitForTopic;
+
+    //! Topic to wait for
+    string topic;
+
+    //! rng
+    boost::mt19937 rng;
+public:
+    RandomTorqueApplier() :
+        pnh("~")
+    {
+        gazeboClient = nh.serviceClient<gazebo_msgs::ApplyBodyWrench>("/gazebo/apply_body_wrench", true /* persistent */);
+        notifier = nh.advertise<std_msgs::Header>("/human/fall", 1, true);
+        pnh.param("x", x, 0.0);
+        pnh.param("y", y, 0.0);
+        pnh.param("z", z, 0.0);
+        pnh.param("duration", duration, DURATION_DEFAULT);
+        pnh.param("waitForTopic", waitForTopic, true);
+        pnh.param("random", random, false);
+        pnh.param<string>("topic", topic, "/balancer/torques");
+
+        if (waitForTopic)
+        {
+            ros::service::waitForService(topic);
+        }
+
+        if (random)
+        {
+            unsigned int seed = 0;
+#if(!USE_FIXED_SEED)
+            seed = static_cast<unsigned int>(std::time(NULL));
+#else
+            const char* scenarioNumberStr = std::getenv("i");
+            unsigned int scenarioNumber = 0;
+            if(scenarioNumberStr != NULL)
+            {
+                scenarioNumber = boost::lexical_cast<unsigned int>(scenarioNumberStr);
+                seed = scenarioNumber + 1000;
+            }
+            else
+            {
+                cout << "No scenario number set. Using 0" << endl;
+                seed = FIXED_SEED;
+            }
+#endif
+            rng.seed(seed);
+
+
+            bool found = false;
+            while (!found)
+            {
+                // X generator
+                boost::uniform_real<double> xRange(0, 200);
+                boost::variate_generator<boost::mt19937&, boost::uniform_real<double> > genX(rng, xRange);
+                x = genX();
+
+                // Y generator
+                boost::uniform_real<double> yRange(-150, 150);
+                boost::variate_generator<boost::mt19937&, boost::uniform_real<double> > genY(rng, yRange);
+                y = genY();
+                if (fabs(x) >= 100 || fabs(y) >= 100)
+                {
+                    found = true;
+                }
+            }
+        }
+    }
+
+    void apply()
+    {
+    	ROS_INFO("Applying forces [%f, %f, %f]", x, y, z);
+        gazebo_msgs::ApplyBodyWrench wrench;
+        wrench.request.body_name = "human::link";
+        wrench.request.wrench.torque.x = x;
+        wrench.request.wrench.torque.y = y;
+        wrench.request.wrench.torque.z = z;
+        wrench.request.duration = ros::Duration(duration);
+
+        if (!gazeboClient.call(wrench))
+        {
+           ROS_ERROR("Failed to apply wrench");
+           return;
+        }
+
+        ROS_DEBUG("Notifying");
+        std_msgs::Header header;
+        header.stamp = ros::Time::now();
+        notifier.publish(header);
+        ROS_DEBUG("Torque application complete");
+    }
+};
+}
+
+int main(int argc, char** argv)
+{
+    ros::init(argc, argv, "random_torque_applier");
+    RandomTorqueApplier rta;
+    rta.apply();
+    ros::Duration(3.0).sleep();
+}
